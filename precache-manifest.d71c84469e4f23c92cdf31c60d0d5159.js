f7d0e4686862aa7fa1f92638e00b8696e2d580ac
Rahul <rahulhambarde300@gmail.com>
2020-05-11 21:41:33 +0530

    Updates

---
+++ precache-manifest.d71c84469e4f23c92cdf31c60d0d5159.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de152c74de7fee2e9b0c877ab2ed2e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68046e2e28a8ad360f2c",
+    "url": "/static/css/2.c837e8ca.chunk.css"
+  },
+  {
+    "revision": "9719f141fe89260dd1da",
+    "url": "/static/css/main.45435c08.chunk.css"
+  },
+  {
+    "revision": "68046e2e28a8ad360f2c",
+    "url": "/static/js/2.e1a5d1cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e1a5d1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9719f141fe89260dd1da",
+    "url": "/static/js/main.df7206de.chunk.js"
+  },
+  {
+    "revision": "a35616d865ebcd47e92d",
+    "url": "/static/js/runtime-main.185a88bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-app-confusion/precache-manifest.97a1c30791b03abffe206411b19aa6b8.js"
+  "/precache-manifest.d71c84469e4f23c92cdf31c60d0d5159.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-app-confusion/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
